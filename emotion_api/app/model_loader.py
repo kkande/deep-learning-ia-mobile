50ab46dad80aeb463c1d56328c20a8cffc64f913
Abdoulaye Kanoute <57998319+kanouteab@users.noreply.github.com>
2026-04-08 17:45:02 -0400

    Update model path and tokenizer in model_loader.py

--- emotion_api/app/model_loader.py
+++ emotion_api/app/model_loader.py
@@ -1,11 +1,12 @@
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import torch
 
-MODEL_PATH = "models"
+MODEL_PATH = "models/distilbert_goemotions_single_label"
+BASE_TOKENIZER = "distilbert-base-uncased"
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH)
+tokenizer = AutoTokenizer.from_pretrained(BASE_TOKENIZER)
 model = AutoModelForSequenceClassification.from_pretrained(MODEL_PATH)
 
 model.to(device)
